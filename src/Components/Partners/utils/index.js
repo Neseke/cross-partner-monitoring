83cd0f1828443c361944679007b62f61be10fa37
Lukas Höpfner <lukas.hoepfner@zlyde.com>
2018-04-23 15:11:24 +0200

    get react-router Link via own function. We probably want an similar function for creating an iframe in the future.

--- src/Components/Partners/utils/index.js
+++ src/Components/Partners/utils/index.js
@@ -4,30 +4,34 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 import partners from '../../../partners.json';
 
 /**
+ * @param { String } url - Where the url should direct to
+ * @returns { Link } - A valid react-router Link
+ */
+function createReactRouterLink(url) {
+  return (
+    <Router>
+      {/* we don't want a state, so we can simply reload the window to match the
+      url in the browser window */}
+      <Link to={url} onClick={() => window.location.reload()}>
+        {`athletia.net${url}`}
+      </Link>
+    </Router>
+  );
+}
+
+/**
  * @param { Array } partners - The partners that will appear in the partner table and
  * for which we need a report.
  * @returns { Array } - A copy of partners, but instead of an url string we now have
  * a react-router Link.
  */
-function createReactRouterLink(partners = []) {
+export function getPartners() {
   return partners.map(partner => {
     return {
       ...partner,
-      url: (
-        <Router>
-          <Link to={partner.url} onClick={() => window.location.reload()}>
-            {`athletia.net${partner.url}`}
-          </Link>
-        </Router>
-      )
+      url: createReactRouterLink(partner.url)
     };
   });
 }
 
-export function getPartners() {
-  return createReactRouterLink(partners);
-}
-
-export default {
-  getPartners
-};
+export default getPartners;
