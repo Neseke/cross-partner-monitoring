501ddf3075c1f91021c9f32f0944a39d8841d9ce
Lukas Höpfner <lukas.hoepfner@zlyde.com>
2018-04-23 17:27:29 +0200

    implemented generic routes ready-to-test

--- src/Components/Navigation/Overview.jsx
+++ src/Components/Navigation/Overview.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Partner from '../Partners/Partner';
 import Dashboard from '../Dashboard';
 
+import { getPartners } from '../Partners/utils/partners';
+
 export default class Overview extends Component {
   render() {
+    const partners = getPartners();
     return (
       <Router>
         <div>
           <Route path="/partners" component={Partner} />
-          <Route path="/reports/ath" component={Dashboard} />
-          <Route path="/reports/ehf" component={Dashboard} />
+          {partners.map(partner => (
+            <Route path={`/reports/${partner.uri}`} component={Dashboard} />
+          ))}
         </div>
       </Router>
     );
