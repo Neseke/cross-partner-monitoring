4635e1e27ad6248d71f7d6b914844e3aeb7a1508
Lukas Höpfner <lukas.hoepfner@zlyde.com>
2018-04-26 14:43:26 +0200

    abolished warning 'can only update a mounted or mounting component'

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: this.isLoggedIn()
+      isLoggedIn: false
     };
 
     this.onSuccessfulLogin = this.onSuccessfulLogin.bind(this);
